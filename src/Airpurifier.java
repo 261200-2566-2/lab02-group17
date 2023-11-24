c6db9235da1533bcc7b0384eae03ce79c9c37539
asporQ <120199524+asporQ@users.noreply.github.com>
2023-11-24 23:21:20 +0700

    fix class method

--- src/Airpurifier.java
+++ src/Airpurifier.java
@@ -2,6 +2,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Airpurifier extends Sensor{
+    //inheritance variable
     String brand = "Xiaomi";
     String model;
     String serialNo;
@@ -9,9 +10,11 @@ public class Airpurifier extends Sensor{
     String mode;
     String filterType; 
 
+    //class variable
     static int manufacturedCount = 0;//สมมติว่าโรงงานพึ่งเปิดตัว ยังไม่เคยผลิต
     static Set<String> modelSet = new HashSet<>();
 
+    //constructor
     Airpurifier(String model, String serialNo) {
         this.model = model;
         this.serialNo = serialNo;
@@ -24,12 +27,13 @@ public class Airpurifier extends Sensor{
         modelSet.add(model);
     }
     
+    //inheritance method
     void turnOn() {status = true;}
     void turnOff() {status = false;}
     double powerConsumption() {
         double powerConsumption = 0.0;
         if(status == true){
-            // calculate
+            // calculate 
             return powerConsumption;
         }
         return powerConsumption;
@@ -48,6 +52,20 @@ public class Airpurifier extends Sensor{
                 break;
         }
     }
+    
+    void getData(Airpurifier purifier1) {        
+        System.out.println("===========================");
+        System.out.println("Brand: " + purifier1.brand);
+        System.out.println("Model: " + purifier1.model);
+        System.out.println("Status: " + (purifier1.status ? "On" : "Off"));
+        System.out.println("Mode: " + purifier1.mode);  
+        System.out.println("Temperature: " + purifier1.temp + "°C");
+        System.out.println("PM2.5 Level: " + purifier1.pmLevel );
+        System.out.println("Filter Type: " + purifier1.filterType );
+        System.out.println("===========================");
+    }
+    
+    //class method
     static String mostPopularModel() {
         String mostPopularModel = null;
         int max = 0;
@@ -66,15 +84,4 @@ public class Airpurifier extends Sensor{
         return mostPopularModel;
     }
 
-    void getData(Airpurifier purifier1) {        
-        System.out.println("===========================");
-        System.out.println("Brand: " + purifier1.brand);
-        System.out.println("Model: " + purifier1.model);
-        System.out.println("Status: " + (purifier1.status ? "On" : "Off"));
-        System.out.println("Mode: " + purifier1.mode);  
-        System.out.println("Temperature: " + purifier1.temp + "°C");
-        System.out.println("PM2.5 Level: " + purifier1.pmLevel );
-        System.out.println("Filter Type: " + purifier1.filterType );
-        System.out.println("===========================");
-    }
 }
